c7904dd9e5d7405bd7b4ad3d9bbfd436680a6491
Kristoffer-ProgGod <Kristoffer.frederichsen@gmail.com>
2020-10-21 09:16:21 +0200

    Second test commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
 
 /*
 this is the main class for our waterworks project
+used to call all other classes
  */
 
 public class Main {
+
 }
+
+
+
+/*
+This is where the Update Consumer Database function will be
+ */
+
+
+
+
+/*
+This is where the Calculate Settlements function will be
+ */
+
+
+/*
+This is where the Notify Consumer function will be
+ */

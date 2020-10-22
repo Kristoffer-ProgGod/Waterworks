a3b3bae58fc4e6b4cf517b8b6c1cf44c18f89e0c
monir <moni1438@easv365.dk>
2020-10-22 11:58:59 +0200

    Monir reminder card

---
+++ src/GiroCard.java
@@ -0,0 +1,31 @@
+public class GiroCard {
+    public static void main(String[] args) {
+        System.out.println(" -------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("| TRANSFER FROM ACCOUNT NUMBER:                                                                                                             |");
+        System.out.println("| |X||X||X||X| |X||X||X||X||X||X||X||X||X||X|                  GIRO PAYMENT                                        RECEIPT                  |");
+        System.out.println("|----------------------------------------------|PAYMENT CONSUMERS ACCOUNT NR AND DESIGNATION|-----------------------------------------------|");
+        System.out.println("| YOU CAN NOT APPLY A                          |                   47848359                 |                       47848359                |");
+        System.out.println("| MESSAGE TO THIS PAYMENT.                     |               UPTOWN WATERWORKS            |                   UPTOWN WATERWORKS           |");
+        System.out.println("|                                              |               SONDERBORG STREET 5          |                   SONDERBORG STREET 5         |");
+        System.out.println("| YOU CAN ASSIGN UR FUTURE                     |               6400 SONDERBORG              |                   6400 SONDERBORG             |");
+        System.out.println("| PAYMENTS TO A PAYMENT SERVICE                |                                            |                                               |");
+        System.out.println("|______________________________________________|____________________________________________|_______________________________________________|");
+        System.out.println("| THE PAYER       :                            | SIGNATURE FROM YOUR OWN BANK ACCOUNT       | SIGNATURE FROM UPTOWN WATERWORKS              |");
+        System.out.println("|                                              |  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~        | ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~      |");
+        System.out.println("| CONSUMER-number : 0000222111234534           |                                            |                                               |");
+        System.out.println("| PBS-number      :   00222112-12345           |                                            |                                               |");
+        System.out.println("|                                              |                                            |                                               |");
+        System.out.println("|                                              |                                            |                                               |");
+        System.out.println("|______________________________________________|____________________________________________|_______________________________________________|");
+        System.out.println("|---DOLLARS---|----CENTS----|------------------|PAYMENT DATE----OR------PAY NOW-------------|-----------------------------------------------|");
+        System.out.println("|    XXXX     |    XXXX     |                  | 12.02.07               |     |             |                                               |");
+        System.out.println("|             |             |                  |                        |     |             |                                               |");
+        System.out.println("|_____________|_____________|__________________|________________________|_____|_____________|_______________________________________________|");
+        System.out.println("|                                              |                                            |                                               |");
+        System.out.println("| +15< 00002221112345300004+445897<            |                                            |          CONSUMER-number : 0000222111234534   |");
+        System.out.println("|                                              |                                            |          PBS-number      :   00222112-12345   |");
+        System.out.println("|______________________________________________|____________________________________________|_______________________________________________|");
+
+
+    }
+}
---
+++ src/ReminderCard.java
@@ -0,0 +1,32 @@
+class cards {
+    public static void main(String[] args) {
+        System.out.println(" ---------------------------------------------------------------------------------------------------------");
+        System.out.println("| UPTOWN WATERWORKS                                                           YOUR ACCOUNT NR.  : XXXXXXXX|");
+        System.out.println("|                                                                             BILL NUMBER       : XXXXXXXX|");
+        System.out.println("|                                                                             DATE OF THIS BILL : DD.MM.YY|");
+        System.out.println("|                                                                             REMINDER NR.      : XXXXXXXX|");
+        System.out.println("|                                                                             PAGE X OUT OF X             |");
+        System.out.println("| HELLO *INSERT NAME*                                                                                     |");
+        System.out.println("| WE ARE SENDING YOU THIS REMINDER BECAUSE OF THE PAYMENT WAS NOT PAYED WITHIN THE GIVEN DEADLINE         |");
+        System.out.println("|                                                                                                         |");
+        System.out.println("|                                                                                                         |");
+        System.out.println("| SPECIFICATIONS                                                                                          |");
+        System.out.println("|---------------------------------------------------------------------------------------------------------|");
+        System.out.println("| SETTLEMENT FEE:                               DURATION:                                 AMOUNT:         |");
+        System.out.println("|---------------------------------------------------------------------------------------------------------|");
+        System.out.println("| *INSERT SETTLEMENTS*                          DD.MM.YY                                  XXXXXX          |");
+        System.out.println("|---------------------------------------------------------------------------------------------------------|");
+        System.out.println("| AMOUNT IN TOTAL                                                                         XXXXXX----------|");
+        System.out.println("|---------------------------------------------------------------------------------------------------------|");
+        System.out.println("| THIS PAYMENTS DEADLINE IS DD.MM.YY.                                                                     |");
+        System.out.println("| IF WE HAVE NOT RECEIVED THE PAYMENT WITHIN THE GIVEN TIME PERIOD                                        |");
+        System.out.println("| A NEW FEE WILL BE ADDED.                                                                                |");
+        System.out.println("| WE WILL CLOSE THE WATER IF PAYMENT IS NOT MET AT ALL.                                                   |");
+        System.out.println("| IF YOU HAVE ANY QUESTIONS PLEASE CONTACT:                                                               |");
+        System.out.println("| - CONTACT US ON OUR PHONE NUMBER: XXXXXXXXXXXX                                                          |");
+        System.out.println("| - CONTACT US ON OUR MAIL ADDRESS: XXXXXXXXXXXX                                                          |");
+        System.out.println("___________________________________________________________________________________________________________");
+    }
+
+
+}
